43406b38b6b64e3d53eecef62223b88177169690
KungadDzingad <51488141+KungadDzingad@users.noreply.github.com>
2019-10-29 21:26:00 +0100

    Add files via upload

---
+++ Graf.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+//NIGDY NIE UZYWAMY DWA RAZY FUNKCJI W JEDNEJ LINII BO SIE SPIERDOLI
+class Graf {
+	std::vector<Funkcja> wszystkie_funkcje_we_wszystkich_plikach;
+	std::vector<Plik> pliki;
+
+	void znajdz_pliki();     //                                                            || -> Patryk
+	void policz_linijki_kodu_dla_pliku(); //liczy linijki kodu dla pliku                   || -> Ewelina
+	void znajdz_polaczenia_pomiedzy_plikami();  //znajduje polaczenia pomiedzy plikami     || -> Patryk
+	void znajdz_definicje_funkcji();			//										   || -> Wojtek
+	void znajdz_funkcje_i_wagi_pomiedzy_plikami();//                                       || -> Bartek
+	void znajdz_polaczenia_miedzy_funkcjami();                 //                          || -> Matuesz Witkowski
+
+	void znajdz_wagi_pomiedzy_funkcjami(); //                                              || -> Matuesz Witkowski
+
+	std::string zamien_na_string_dla_plikow_do_grafu();  //                                || -> Matuesz Wicherski
+	std::string zamien_na_string_dla_funkcji_do_grafu(); //do skopiowania i prxerobienia   || -> Matuesz Wicherski
+	void rysuj_graf(std::string do_rysowania_grafu, std::string nazwa_grafu);//GOTOWE      || -> Matuesz Wich i Wojtek
+};
+
+struct Plik {
+	std::string nazwa_pliku;
+	int ilosc_linijek_kodu;
+	std::vector<Polaczenie> polaczenia_miedzy_plikami;
+	std::vector<Funkcja> funkcje;
+	Plik(std::string);
+};
+
+struct Polaczenie {
+	std::string nazwa_polaczonego_elementu;
+	int waga;   //zalezne od liczby wywolan funkcji
+	Polaczenie(std::string);
+};
+
+struct Funkcja {
+	std::string nazwa_funkcji;
+	std::vector<Polaczenie> polaczenia_miedzy_funkcjami;
+	Funkcja(std::string);
+};
