b16dbe0d3d5010addba289fb265da19392c48976
pazindor <56399197+pazindor@users.noreply.github.com>
2019-12-07 12:03:54 +0100

    Delete odcinanie_poczatku.cpp

--- odcinanie_poczatku.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Funkcje_od_stringow.h"
-
-std::string odcinanie_poczatku(std::string linia, std::string poczatek) {
-	size_t pozycja = linia.find(poczatek);
-	linia = linia.erase(0, pozycja + poczatek.length());
-	return linia;
-}
